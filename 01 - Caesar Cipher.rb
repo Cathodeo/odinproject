60e2c070237c2c0b7b00c180bfc21e396e0b6014
Tadeo Marin <114956257+Cathodeo@users.noreply.github.com>
2024-07-28 17:16:35 +0200

    Add files via upload

---
+++ 01 - Caesar Cipher.rb
@@ -0,0 +1,15 @@
+def caesar_cipher(sentence, factor)
+  decomposed = sentence.chars
+  num_values = decomposed.map {|char| char.ord}
+  shifted_numbers = num_values.map do |number|
+    case number 
+    when 65..(90 - factor) then number = number + factor
+    when (91 - factor)..90 then number = (number - 26) + factor  
+    when 97..(122 - factor) then number = number + factor
+    when (123 - factor)..122 then number = (number - 26) + factor
+    else number = number
+    end
+  end 
+  shifted_chars = shifted_numbers.map {|number| number.chr}
+  shifted_string = shifted_chars.join
+end
