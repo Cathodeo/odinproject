0d2faeaaaf8be863771f93eb206c968ac2fd600c
tmmartin <personal@tmmartin.eu>
2024-07-29 22:38:46 +0200

    Test commit from RubyMine

--- 02 - Sub Strings.rb
+++ 02 - Sub Strings.rb
@@ -1,2 +1,14 @@
-# frozen_string_literal: true
+dictionary = ["actually", "regards", "well", "hope", "unfortunately", "issue", "message", "apologies"]
+def substrings(word_collection, dictionary)
 
+  matches = []
+  word_divided = word_collection.split
+  word_divided.map do |word|
+    if dictionary.include?(word.downcase)
+      matches << word.downcase
+    end
+  end
+
+  return matches
+
+end
