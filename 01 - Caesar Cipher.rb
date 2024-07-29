f085cef385a351bd7527da17381b6794a8395d57
tmmartin <personal@tmmartin.eu>
2024-07-29 21:53:29 +0200

    Test commit from RubyMine

--- 01 - Caesar Cipher.rb
+++ 01 - Caesar Cipher.rb
@@ -3,13 +3,13 @@ def caesar_cipher(sentence, factor)
   num_values = decomposed.map {|char| char.ord}
   shifted_numbers = num_values.map do |number|
     case number 
-    when 65..(90 - factor) then number = number + factor
-    when (91 - factor)..90 then number = (number - 26) + factor  
-    when 97..(122 - factor) then number = number + factor
-    when (123 - factor)..122 then number = (number - 26) + factor
-    else number = number
+    when 65..(90 - factor) then number + factor
+    when (91 - factor)..90 then (number - 26) + factor  
+    when 97..(122 - factor) then number + factor
+    when (123 - factor)..122 then (number - 26) + factor
+    else number
     end
   end 
   shifted_chars = shifted_numbers.map {|number| number.chr}
-  shifted_string = shifted_chars.join
+  shifted_chars.join
 end
